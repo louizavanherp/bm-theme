453b30ddded87dbaabf0b3722978355ddca985b3
louizavanherp <louiza.vanherp@gmail.com>
2019-09-30 15:38:40 +0200

    cases content

--- front-page.php
+++ front-page.php
@@ -1,9 +1,8 @@
 <?php get_header();?>
 
-    <div class="container">
 
             <?php $intro = get_field('intro');?>
-            <section class="intro">
+            <section class="intro container">
                 <div class="row">
                     <div class="intro__img col">
 
@@ -20,12 +19,8 @@
                     <div class="intro__copy col">
                         <h1 class="intro__heading"><?php echo $intro['large_title']; ?></h1>
                         <p class="intro__txt"><?php echo $intro['main_text']; ?></p>
-                        <?php 
-
-                        if( !empty($intro['signature']) ): ?>
-
-                            <img src="<?php echo $intro['signature']['sizes']['thumbnail']; ?>" alt="<?php echo $intro['signatre']['alt']; ?>" />
-
+                        <?php if( !empty($intro['signature']) ): ?>
+                        <img src="<?php echo $intro['signature']['sizes']['thumbnail']; ?>" alt="<?php echo $intro['signatre']['alt']; ?>" />
                         <?php endif; ?>
                     </div>
                 </div>
@@ -33,7 +28,7 @@
             </section>
 
             <?php $services = get_field('services');?>
-            <section class="services">
+            <section class="services container" id="services">
                 <h1><?php echo $services['title']; ?></h1>
 
                 <div class="row">
@@ -61,9 +56,65 @@
                 </div>
             </section>
 
-            <p id="test">Dit is een test </p>
+            <?php $cases = get_field('cases');?>
+            <section class="cases container">
+                <div class="row">
+                    <div class="col">
+                    <?php if( !empty($cases['image_1']) ): ?>
+                    <img src="<?php echo $cases['image_1'] ?>" alt="<?php echo $cases['image_1']['alt']; ?>" />
+                    <?php endif; ?>
+                    </div>
+                    <div class="col-md-auto">
+                        <hr class="line">
+                    </div>
+                    <div class="col col-lg-2">
+                    <?php if( !empty($cases['title_1']) ): ?>
+                        <p><?php echo $cases['title_1'] ?></p>
+                    <?php endif; ?>
+                    <?php if( !empty($cases['link_1']) || !empty($cases['link_text']) ): ?>
+                        <a href="<?php echo $cases['link_1'] ?>"><?php echo $cases['link_text'];?></a>
+                    <?php endif; ?>
+                    </div>
+                </div>
 
-    </div>
+                <div class="row">
+                    <div class="col">
+                    <?php if( !empty($cases['image_2']) ): ?>
+                    <img src="<?php echo $cases['image_2'] ?>" alt="<?php echo $cases['image_2']['alt']; ?>" />
+                    <?php endif; ?>
+                    </div>
+                    <div class="col-md-auto">
+                        <hr class="line">
+                    </div>
+                    <div class="col col-lg-2">
+                    <?php if( !empty($cases['title_2']) ): ?>
+                        <p><?php echo $cases['title_2'] ?></p>
+                    <?php endif; ?>
+                    <?php if( !empty($cases['link_2']) || !empty($cases['link_text']) ): ?>
+                        <a href="<?php echo $cases['link_2'] ?>"><?php echo $cases['link_text'];?></a>
+                    <?php endif; ?>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                    <?php if( !empty($cases['image_3']) ): ?>
+                    <img src="<?php echo $cases['image_3'] ?>" alt="<?php echo $cases['image_3']['alt']; ?>" />
+                    <?php endif; ?>
+                    </div>
+                    <div class="col-md-auto">
+                        <hr class="line">
+                    </div>
+                    <div class="col col-lg-2">
+                    <?php if( !empty($cases['title_3']) ): ?>
+                        <p><?php echo $cases['title_3'] ?></p>
+                    <?php endif; ?>
+                    <?php if( !empty($cases['link_3']) || !empty($cases['link_text']) ): ?>
+                        <a href="<?php echo $cases['link_3'] ?>"><?php echo $cases['link_text'];?></a>
+                    <?php endif; ?>
+                    </div>
+                </div>
+            </section>
 
 
 
