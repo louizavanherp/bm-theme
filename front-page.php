db0927ac3aea64c05b206983eea7d69f9a3f94b5
louizavanherp <louiza.vanherp@gmail.com>
2019-09-30 09:09:39 +0200

    structure intro section

--- front-page.php
+++ front-page.php
@@ -1,4 +1,24 @@
 <?php get_header();?>
 
 
+
+    <div class="container">
+
+        <?php $intro = get_field('intro_');?>
+        <section class="intro">
+            <div class="intro__img">
+                <img src="" alt="">
+            </div>
+
+            <div class="intro__copy">
+                <h1 class="intro__heading"><?php echo $intro['large_title_']; ?></h1>
+                <p class="intro__txt"><?php echo $intro['main_text_']; ?></p>
+                <img src="" alt="" class="intro__signature">
+            </div>
+        </section>
+
+    </div>
+
+
+
 <?php get_footer();?>
