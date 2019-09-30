8e457e0c7a15605b215aa44119e79b0e45c1e85d
louizavanherp <louiza.vanherp@gmail.com>
2019-09-30 10:18:22 +0200

    lees meer link

--- front-page.php
+++ front-page.php
@@ -1,39 +1,35 @@
 <?php get_header();?>
 
-
-
     <div class="container">
 
-        
-
-
-        <div class="container">
-
             <?php $intro = get_field('intro');?>
-            <section class="intro row">
-                <div class="intro__img col">
+            <section class="intro">
+                <div class="row">
+                    <div class="intro__img col">
 
-                    <?php 
+                        <?php 
 
-                    if( !empty($intro['image']) ): ?>
+                        if( !empty($intro['image']) ): ?>
 
-                        <img src="<?php echo $intro['image']['sizes']['medium']; ?>" alt="<?php echo $intro['image']['alt']; ?>" />
+                            <img src="<?php echo $intro['image']['sizes']['medium']; ?>" alt="<?php echo $intro['image']['alt']; ?>" />
 
-                    <?php endif; ?>
+                        <?php endif; ?>
 
+                    </div>
+
+                    <div class="intro__copy col">
+                        <h1 class="intro__heading"><?php echo $intro['large_title']; ?></h1>
+                        <p class="intro__txt"><?php echo $intro['main_text']; ?></p>
+                        <?php 
+
+                        if( !empty($intro['signature']) ): ?>
+
+                            <img src="<?php echo $intro['signature']['sizes']['thumbnail']; ?>" alt="<?php echo $intro['signatre']['alt']; ?>" />
+
+                        <?php endif; ?>
+                    </div>
                 </div>
-
-                <div class="intro__copy col">
-                    <h1 class="intro__heading"><?php echo $intro['large_title']; ?></h1>
-                    <p class="intro__txt"><?php echo $intro['main_text']; ?></p>
-                    <?php 
-
-                    if( !empty($intro['signature']) ): ?>
-
-                        <img src="<?php echo $intro['signature']['sizes']['thumbnail']; ?>" alt="<?php echo $intro['signatre']['alt']; ?>" />
-
-                    <?php endif; ?>
-                </div>
+                <a href="<?php echo $intro['read_more_link'] ?>"><?php echo $intro['read_more_link_text'];?></a>
             </section>
 
             <?php $services = get_field('services');?>
@@ -65,7 +61,7 @@
                 </div>
             </section>
 
-        </div>
+            <p id="test">Dit is een test </p>
 
     </div>
 
