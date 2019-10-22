07c6b0508a7b1939f2399addf0de64b2ea33f669
louizavanherp <louiza.vanherp@gmail.com>
2019-10-22 19:32:44 +0200

    changed css path + added sass

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ function load_stylesheets()
     wp_register_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css', array(), false, 'all');
     wp_enqueue_style('bootstrap');
 
-    wp_register_style('style', get_template_directory_uri() . '/style.css', array(), false, 'all');
+    wp_register_style('style', get_template_directory_uri() . '/style/style.css', array(), false, 'all');
     wp_enqueue_style('style');
 }
 add_action('wp_enqueue_scripts', 'load_stylesheets');
