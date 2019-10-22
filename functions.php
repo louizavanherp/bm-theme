90be1be4383c2f53fa36cf184474d9e5265f68cd
louizavanherp <louiza.vanherp@gmail.com>
2019-10-22 20:41:59 +0200

    added poppins google font

--- functions.php
+++ functions.php
@@ -9,6 +9,8 @@ function load_stylesheets()
 
     wp_register_style('style', get_template_directory_uri() . '/style/style.css', array(), false, 'all');
     wp_enqueue_style('style');
+
+    wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=Poppins:400,500,700&display=swap', false );
 }
 add_action('wp_enqueue_scripts', 'load_stylesheets');
 
@@ -37,9 +39,3 @@ register_nav_menus(
         'footer-menu' => __('Footer Menu', 'theme'),
     )
 );
-
-/*google fonts*/
-function add_google_fonts() {
-    wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=Poppins:400,500,700&display=swap', false );
-    }
-    add_action( 'wp_enqueue_scripts', 'add_google_fonts' );
